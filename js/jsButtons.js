f701422b67152fc23104716974d6a0fee044c070
Katiaa <katyakravchuk123@gmail.com>
2021-07-14 20:57:43 +0300

    added js files

---
+++ js/jsButtons.js
@@ -0,0 +1,23 @@
+let goBackButton = document.getElementById('jsGoBack');
+if (goBackButton) 
+{
+    goBackButton.addEventListener('click', goBack);
+}
+
+function goBack(event)
+{
+    window.history.back();
+}
+
+//---------- got to product ----------------
+let productsImg = document.getElementsByClassName('jsProductImg');
+for (let i = 0; i < productsImg.length; i++)
+{
+    productsImg[i].addEventListener('click', productDetails);
+}
+
+function productDetails(event)
+{
+    let product = event.target;
+    window.location = 'product.html?productName=' + product.alt;
+}
